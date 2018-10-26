a37cb13719b4cebc2aa951666de834e6d1afc007
Rose Peruski <rose.peruski@gmail.com>
2018-10-25 22:26:10 -0400

    multiline string

--- me.py
+++ me.py
@@ -1,4 +1,10 @@
-me="Rose";
-print(me);
-add_up = 3 + 7;
-print(add_up);
+me="Rose"
+print(me)
+add_up = 3 + 7
+print(add_up)
+modulo_test = 25%5
+print("This is a modulo test " + str(modulo_test));
+
+multiline_string ="""Stranger, if you passing meet me and desire to speak to me, why should you not speak to me? Andy why should I not speak to you?
+"""
+print(multiline_string)
